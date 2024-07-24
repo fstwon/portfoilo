f4e926f1837097d14d86bed1497128c74962fcbf
fstwon <fstwon@gmail.com>
2024-07-24 18:17:38 +0900

    온보딩 페이지, 메인 페이지 컴포넌트 추가

--- src/App.component.tsx
+++ src/App.component.tsx
@@ -1,12 +1,33 @@
+import { useSelector } from "react-redux";
+import { OnboardingPage } from "./pages/onboarding/Onboarding.page";
+import { MainPage } from "./pages/main/main.page";
+import { RootState } from "./redux/Root.redux";
+import { TextLogo } from "./pages/onboarding/components/textLogo/TextLogo.component";
+import { useEffect, useRef, useState } from "react";
 import "./app.styles.scss";
 
+type TIsDoneOnboarding = boolean;
+
 function App() {
+  const { IS_INIT_RENDER } = useSelector((state: RootState) => state.AppReducer);
+  const [isDoneOnboarding, setIsDoneOnboarding] = useState<TIsDoneOnboarding>(() => false)
+  const onBoardingTimeoutId = useRef<ReturnType <typeof setTimeout>>()
+
+  useEffect(() => {
+    if(!IS_INIT_RENDER) {
+      onBoardingTimeoutId.current = setTimeout(() => {
+        setIsDoneOnboarding(() => true)
+        clearTimeout(onBoardingTimeoutId.current)
+      }, 2300)
+    }
+  }, [IS_INIT_RENDER])
 
   return (
     <div className="App" >
-      
+      {!isDoneOnboarding && <OnboardingPage />}
+      <MainPage />
     </div>
   )
-}
+};
 
 export default App;
