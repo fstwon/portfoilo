42113b904c32c99e015665daf99df0561cbb4aa9
fstwon <fstwon@gmail.com>
2024-07-24 18:18:01 +0900

    리덕스 스토어 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.component";
 import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./redux/Root.redux";
+import { Provider } from "react-redux";
+import "./reset.styles.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+const store = configureStore({
+  reducer: rootReducer
+});
 
 root.render(
   <React.StrictMode >
     <BrowserRouter >
-      <App />
+      <Provider store={store} >
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
